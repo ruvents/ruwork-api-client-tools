ba371fba4c1a4a2dddf66831e1a6f2420c1cffa8
Valentin <udaltsov.valentin@gmail.com>
2018-02-08 01:30:51 +0300

    Fixed TestFacade.

--- tests/Fixtures/Facade/TestFacade.php
+++ tests/Fixtures/Facade/TestFacade.php
@@ -6,8 +6,13 @@ use Ruwork\ApiClientTools\Facade\AbstractFacade;
 
 class TestFacade extends AbstractFacade
 {
-    protected $options = [
-        'method' => 'GET',
-        'endpoint' => '/test',
-    ];
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->options = [
+            'endpoint' => '/test',
+        ];
+    }
 }
